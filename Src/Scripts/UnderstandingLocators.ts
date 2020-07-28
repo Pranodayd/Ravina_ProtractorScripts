38aff4fc47d1b2e9a07d908998e9f820a5693f27
Pranoday Dingare <pranoday_dingare@magicsoftware.com>
2020-07-28 06:52:51 +0530

    Doing initial commit

---
+++ Src/Scripts/UnderstandingLocators.ts
@@ -0,0 +1,20 @@
+import {browser} from "protractor";
+//describe is SUITE of testcases
+describe('Check all Arithmetic operations', function() {
+    //it is SINGLE testcase
+    it('Addition testcase', function() {
+      
+      browser.get('http://juliemr.github.io/protractor-demo/');
+        
+      
+    });
+
+    it('Subtraction testcase', function() {
+      
+        
+          
+        
+      });
+
+  });
+  
---
+++ Src/protractorconfig.ts
@@ -0,0 +1,22 @@
+
+export let config = {
+    // The address of a running selenium server.
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
+  
+    // Capabilities to be passed to the webdriver instance.
+    directConnect: true,
+    capabilities: {
+      browserName: 'chrome'
+    },
+  
+    // Spec patterns are relative to the configuration file location passed
+    // to protractor (in this example conf.js).
+    // They may include glob patterns.
+    specs: ['./Scripts/UnderstandingLocators.js'],
+  
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+      showColors: true, // Use colors in the command line report.
+    }
+  };
+  
