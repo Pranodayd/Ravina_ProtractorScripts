d25a94bb7ccd3ce1f38bdf9e523a69e2345f96c8
ravinakalyanshetty@gmail.com <ravinakalyanshetty@gmail.com>
2020-07-29 21:44:20 -0400

    changed package.json and understandingLcators.ts

--- Src/Scripts/UnderstandingLocators.ts
+++ Src/Scripts/UnderstandingLocators.ts
@@ -1,11 +1,11 @@
-import {browser} from "protractor";
+import {browser, element, by} from "protractor";
 //describe is SUITE of testcases
 describe('Check all Arithmetic operations', function() {
     //it is SINGLE testcase
     it('Addition testcase', function() {
       
       browser.get('http://juliemr.github.io/protractor-demo/');
-        
+      element(by.model("first"));  
       
     });
 
