39e6f60bd57a22142c38831729d5d3d9ece61444
Pranoday Dingare <pranoday_dingare@magicsoftware.com>
2020-08-03 08:48:17 +0530

    Added Subtraction Testcase

--- Src/Scripts/UnderstandingLocators.ts
+++ Src/Scripts/UnderstandingLocators.ts
@@ -16,11 +16,20 @@ describe('Check all Arithmetic operations', function() {
     
     });
 
-    it('Subtraction testcase', function() {
+    it('Subtraction testcase', async function() {
       
         
-          
-        
+      element(by.model("first")).sendKeys("5");
+      let AllOptions=await element.all(by.options("value for (key, value) in operators"));
+      console.log("Number of Operators "+AllOptions.length)
+      AllOptions[4].click();
+      
+      element(by.model("second")).sendKeys("2");
+      
+      element(by.cssContainingText("button[class='btn']","Go!")).click();
+      console.log(await element(by.tagName("h2")).getText());
+      console.log(await AllOptions[4].getAttribute("value"));
+      browser.sleep(10000);  
       });
 
   });
