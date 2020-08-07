0728421155b7b8b7442187b63743fabb5f77b62e
Pranoday Dingare <pranoday_dingare@magicsoftware.com>
2020-08-07 07:54:25 +0530

    Modified Repeater.js

--- Src/Scripts/Repeater.ts
+++ Src/Scripts/Repeater.ts
@@ -9,7 +9,7 @@ describe('Check all Arithmetic operations', function() {
       element(by.model("second")).sendKeys("5");
       element(by.cssContainingText("button[class='btn']","Go!")).click();
       console.log(await element(by.tagName("h2")).getText());
-      console.log(await element(await by.repeater('result in memory')).getSize());
+      console.log(await (await element.all(await by.repeater('result in memory'))).length)
       browser.sleep(10000); 
 
    
